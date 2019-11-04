3c6be6ba5adb266f178a73e6cf820af78da2746d
lechalet <54844229+lechalet@users.noreply.github.com>
2019-11-04 11:20:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../src/HighWay.php';
+require_once '../src/MotorWay.php';
+require_once '../src/ResidentialWay.php';
+require_once '../src/PedestrianWay.php';
+require_once '../src/Vehicle.php';
+require_once '../src/Bicycle.php';
+            
